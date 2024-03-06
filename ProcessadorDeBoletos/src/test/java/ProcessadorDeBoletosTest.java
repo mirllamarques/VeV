bdf9e4cb422d6fbec8e9d5e9d7879727c33cddcc
Otávio Cipriano <mirllamsa@gmail.com>
2024-03-06 20:30:26 -0300

    add Maven

---
+++ ProcessadorDeBoletos/src/test/java/ProcessadorDeBoletosTest.java
@@ -0,0 +1,26 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ProcessadorDeBoletosTest {
+
+    @Test
+    void adicionaBoleto() {
+    }
+
+    @Test
+    void getBoletos() {
+    }
+
+    @Test
+    void setValorBoletos() {
+    }
+
+    @Test
+    void mudaStatus() {
+    }
+
+    @Test
+    void extrato() {
+    }
+}
